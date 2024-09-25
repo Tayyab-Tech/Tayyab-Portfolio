b79dc02248f032143c6b67007a3bcb609b8d238a
Tayyab-Tech <tayyabtechnology@gmail.com>
2024-09-25 19:15:53 +0500

    updat

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <>
       <Header YourNameLogo="TS" YourName="Tayyab."/>
 
-      <Home Skill1="Front-end Devloper" Skill2="Web-Designer" YourInfo="A passionate Front-end React Developer and fully Responsive Designer based in Lahore, Pakistan. 📍" LinkedIn="" GitHub="https://github.com/Tayyab-Tech" YourImg="/my-img.png" YourImgAlt="my-img" />
+      <Home Skill1="Front-end Devloper" Skill2="Web-Designer" YourInfo="A passionate Front-end React Developer and fully Responsive Designer based in Lahore, Pakistan. 📍" LinkedIn="" GitHub="https://github.com/Tayyab-Tech" YourImg="/src/my-img.png" YourImgAlt="my-img" />
       
       <About AboutHeading="Meet the React Enthusiast behind the Screen. Crafting Dynamic User Interfaces with Passion and Precision." AboutData="I'm a front-end developer skilled in React.js, with a focus on creating responsive, user-friendly web apps. I have expertise in JavaScript, HTML5, CSS3, and frameworks like Tailwind CSS, React Bootstrap, and Bootstrap for clean, scalable designs. I also specialize in converting Figma designs into responsive websites using HTML, CSS3, Tailwind CSS, and JavaScript. I'm dedicated to optimizing performance, ensuring cross-browser compatibility, and delivering seamless digital experiences." TechStackImg1="/Html.png" TechStackImg1Alt="html-logo" TechStackImg2="/Css.png" TechStackImg2Alt="css-logo" TechStackImg3="/Bootstrap.png" TechStackImg3Alt="bootstrap-logo" TechStackImg4="/Tailwind.png" TechStackImg4Alt="tailwind-logo" TechStackImg5="/Javascript.png" TechStackImg5Alt="JS-logo" TechStackImg6="/React.png" TechStackImg6Alt="react-logo" />
       
